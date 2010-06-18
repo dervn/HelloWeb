6f0b610bfd328d2bef92e084cd2874faf57ac50f
pythn <none@none>
2010-06-18 18:42:19 +0800

    a

--- controllers/index.py
+++ controllers/index.py
@@ -2,11 +2,18 @@
 # coding: utf-8
 
 from settings import render
+from settings import db
 
 
 class Index:
 
     def GET(self):
+        entries = db.select('table1')
+        ret = '<table>'
+        if entries:        
+                for entry in entries:
+                        ret += '<tr><td>'+ str(entry['id'])+'</td>'+entry['name']+'</tr>'  
+        ret += '</table>'              
         return render.index()
         # 请求 index 模板，可以按目录结构来写
         # 如 rendr.default.index()，代表请求 default 目录下的 index 模板
--- settings.py
+++ settings.py
@@ -3,6 +3,12 @@
 
 import web
 
+web.config.debug = True
+web.webapi.internalerror = web.debugerror
 
 render = web.template.render('templates/', cache=False) # 设置模板目录和模板缓存
 web.template.Template.globals['static'] = '/static' # 在模板中可以使用 static 该全局对象
+
+#db = web.database(dbn='mysql', user='username', pw='password', db='dbname')
+
+db = web.database(dbn='sqlite', db='db/test.sqlite')
