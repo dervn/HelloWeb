b4a58e2e2e56a42424473790f785eaeb67212bcc
pythn <waituy@gmail.com>
2010-06-21 10:58:59 +0800

    hello

--- core/controllers/hello.py
+++ core/controllers/hello.py
@@ -10,7 +10,7 @@ class Index:
     def GET(self):
         entries = db.select('table1')
         name = 'Bob'
-        return render.index(name,entries)
+        return render.hello.index(name,entries)
         # 请求 index 模板，可以按目录结构来写
         # 如 rendr.default.index()，代表请求 default 目录下的 index 模板
         # 模板的扩展名有要求，.html 或者 .xml 都可以
--- core/url.py
+++ core/url.py
@@ -4,4 +4,5 @@
 urls = (
     '/',        'index.Index',
     # 该行代表用户请求根目录时是去请求 index 控制器对象的 Index 方法
+    '/hello',   'hello.Index',    
 )
