0307af9d519f28a1be9b0df20f8f6cce241199da
pythn <waituy@gmail.com>
2010-06-21 18:24:55 +0800

    count

--- core/controllers/cook.py
+++ core/controllers/cook.py
@@ -3,12 +3,14 @@
 
 from core.config.settings import render
 from core.config.settings import mysqldb
+from webhelpers.paginate import Page
 
 pageSize = 10
 class Index:
 
     def GET(self):        
         entries = mysqldb.select('cp',what='id,cm,td,yl,zzff',limit=pageSize)
+        from webhelpers.paginate import Page
         return render.cook.index(entries) 
     
 class List:
@@ -17,6 +19,8 @@ class List:
         if not page:
             page = 1
         entries = mysqldb.select('cp',what='id,cm,td,yl,zzff',where='id<'+str(int(page)*pageSize)+' and id>'+str((int(page)-1)*pageSize),limit=pageSize)
+        count = mysqldb.select('cp',what="count(1) as count")[0].count
+        
         return render.cook.list(entries)    
     
 class View:
